212ba6a1f8c61e04c3a73b5b522cdc5d73af6ac0
Sari Andras <sari_andras@phd.ceu.edu>
2025-08-25 15:14:33 +0200

    multiprocessing

--- save_content.py
+++ save_content.py
@@ -5,12 +5,11 @@ import json
 from playwright.sync_api import sync_playwright
 import os
 import time
+import multiprocessing
+import random
 
 INPUT_FOLDER_PATTERN = '../PublisherMetaJSONExporter_2025_07_31/europejson3/**/*.json'
-OUTPUT_FOLDER = 'output/raw/'
-MAX_RETRY = 1
-RETRY_WAIT_TIME = 1
-DIR_MAX_FILES = 300
+
 
 def read_input_urls(json_list, url_list) :
     for json_file in json_list:
@@ -34,13 +33,24 @@ def read_done_urls(urls_set) :
         pass
     return
 
+def read_done_urls_cores(urls_set) :
+    for i in range(20) :
+        try:
+            with open("output/raw_done" + str(i) + ".csv", "r", encoding="utf-8") as text_file:
+                for line in text_file:
+                    print(line)
+                    urls_set.add(line[:-1])
+        except:
+            pass
+    return
+
 def write_done_url(done_url) :
     with open("output/raw_done.csv", "a", encoding="utf-8") as text_file:
         text_file.write(done_url+"\n")
 
-def write_failed_url(failed_url) :
-    with open("output/failed_pages.csv", "a+") as fail:
-        fail.write(str(url)+"\n")
+def write_failed_url(failed_url, core) :
+    with open("output/failed_pages" + str(core) + ".csv", "a+") as fail:
+        fail.write(str(failed_url)+"\n")
 
 def write_problematic_file(failed_file) :
     with open("output/failed_files.csv", "a+") as fail:
@@ -78,89 +88,117 @@ def get_file_counter() :
     end = last_line.find(";")
     return last_line[0:end]
     
+def chunkify(lst,n):
+    return [ lst[i::n] for i in range(n) ]
 
-files = glob.glob(INPUT_FOLDER_PATTERN, recursive=True)
+def save_pages(to_read_urls, core, done_urls) :
+    OUTPUT_FOLDER = 'output/raw/'
+    MAX_RETRY = 1
+    RETRY_WAIT_TIME = 1
+    DIR_MAX_FILES = 300
 
-done_urls = set()
-to_read_urls = []
-read_input_urls(files, to_read_urls)
-read_done_urls(done_urls)
-
-
-
-with sync_playwright() as p:
-    browser = p.chromium.launch(headless=True)
-    with open("output/results.csv", "r") as file:
-        for line in file :
-            pass
-        last_line = line
-        end = last_line.find(";")
-        file_counter =  int(last_line[0:end])
-        last_line = last_line[end+1:]
-        end = last_line.find(";")
-        folder_counter = int(last_line[0:end])
-
-    for url in tqdm(to_read_urls, "Saving webpages...") :
-        if url in done_urls:
-            continue
-        retry = 1
-        retry_count = 0
-        fail = 0
-        while(retry_count < MAX_RETRY and retry == 1) :
-            retry = 0
-            try:
-                page = browser.new_page()
-                response = page.goto(url, wait_until="domcontentloaded", timeout=15000)
-            
-                # Wait until the final navigation settles
-                final_url = page.wait_for_url("**", timeout=15000)
-
-                #SAVE CONTENT
-                folder_path = OUTPUT_FOLDER + str(folder_counter) + "/"
-                if not os.path.exists(folder_path):
-                    os.makedirs(folder_path)
-
-                save_path = folder_path + str(file_counter) + ".csv"
-                with open(save_path, "w", encoding="utf-8") as output_html:
-                    output_html.write(page.content())
-
-                file_counter = file_counter + 1 
-                if file_counter >= DIR_MAX_FILES:
-                    folder_counter = folder_counter + 1
-                    file_counter = 0
-
-            except Exception as e:
-                problem = 1
-                problem_counter = 0
-                #Check if internet or main site problem, wait for it to resolve
-                while(problem):
-                    time.sleep(RETRY_WAIT_TIME)
-                    google_fail = check_google_fail(browser)
-                    main_site_fail  = check_main_site(get_main_site(url), browser)
-                    print("Connection issue: " + str(google_fail))
-                    print("Able to reach " + get_main_site(url) +": " + str(main_site_fail))
-                    problem = google_fail or main_site_fail
-                    
-                    if (main_site_fail and not google_fail):
-                        problem_counter = problem_counter + 1
-                    
-                    if (problem_counter > 1) :
-                        problem = 0
-
-                retry = 1
-                retry_count = retry_count + 1
-                if (retry_count >= MAX_RETRY) :
-                    fail = 1
-                    write_failed_url(url)
-            page.close()
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        try:
+            with open("output/results" + str(core) + ".csv", "r") as file:
+                for line in file :
+                    pass
+                last_line = line
+                end = last_line.find(";")
+                file_counter =  int(last_line[0:end])
+                last_line = last_line[end+1:]
+                end = last_line.find(";")
+                folder_counter = int(last_line[0:end])
+        except:
+            file_counter = 0
+            folder_counter = 0
 
 
-        # SAVE OUTPUT
-        if (fail == 0):
-            with open("output/results.csv", "a", encoding="utf-8") as text_file:
-                text_file.write(str(file_counter)+";"+str(folder_counter)+";"+ url+"\n")
+        for url in tqdm(to_read_urls, "Saving webpages...") :
+            if url in done_urls:
+                continue
+            retry = 1
+            retry_count = 0
+            fail = 0
+            while(retry_count < MAX_RETRY and retry == 1) :
+                retry = 0
+                try:
+                    page = browser.new_page()
+                    response = page.goto(url, wait_until="domcontentloaded", timeout=15000)
+                
+                    # Wait until the final navigation settles
+                    final_url = page.wait_for_url("**", timeout=15000)
 
-            with open("output/raw_done.csv", "a", encoding="utf-8") as text_file:
-                text_file.write(url+"\n")
+                    #SAVE CONTENT
+                    folder_path = OUTPUT_FOLDER + str(folder_counter) + "_" + str(core) + "/"
+                    if not os.path.exists(folder_path):
+                        os.makedirs(folder_path)
+
+                    save_path = folder_path + str(file_counter) + ".csv"
+                    with open(save_path, "w", encoding="utf-8") as output_html:
+                        output_html.write(page.content())
+
+                    file_counter = file_counter + 1 
+                    if file_counter >= DIR_MAX_FILES:
+                        folder_counter = folder_counter + 1
+                        file_counter = 0
+
+                except Exception as e:
+                    problem = 1
+                    problem_counter = 0
+                    #Check if internet or main site problem, wait for it to resolve
+                    while(problem):
+                        time.sleep(RETRY_WAIT_TIME)
+                        google_fail = check_google_fail(browser)
+                        main_site_fail  = check_main_site(get_main_site(url), browser)
+                        print("Connection issue: " + str(google_fail))
+                        print("Able to reach " + get_main_site(url) +": " + str(main_site_fail))
+                        problem = google_fail or main_site_fail
+                        
+                        if (main_site_fail and not google_fail):
+                            problem_counter = problem_counter + 1
+                        
+                        if (problem_counter >= 1) :
+                            problem = 0
+
+                    retry = 1
+                    retry_count = retry_count + 1
+                    if (retry_count >= MAX_RETRY) :
+                        fail = 1
+                        write_failed_url(url, core)
+                page.close()
+
+
+            # SAVE OUTPUT
+            if (fail == 0):
+                with open("output/results" + str(core) + ".csv", "a", encoding="utf-8") as text_file:
+                    text_file.write(str(file_counter)+";"+str(folder_counter)+";"+ url+"\n")
+
+                with open("output/raw_done" + str(core) + ".csv", "a", encoding="utf-8") as text_file:
+                    text_file.write(url+"\n")
+
+
+if __name__ == '__main__' :
+    files = glob.glob(INPUT_FOLDER_PATTERN, recursive=True)
+
+    done_urls = set()
+    to_read_urls = []
+    read_input_urls(files, to_read_urls)
+    read_done_urls(done_urls)
+    read_done_urls_cores(done_urls)
+
+    cores = 6
+    print(str(len(to_read_urls))+"\n") 
+    to_read_urls = list(dict.fromkeys(to_read_urls))
+    print(str(len(to_read_urls))+"\n") 
+    random.shuffle(to_read_urls)
+    lists = chunkify(to_read_urls, cores)
+
+    for i in range(cores):
+        globals()['p_%d' % i]=multiprocessing.Process(target=save_pages, args=(lists[i], i, done_urls))
+        globals()['p_%d' % i].start()
+    for i in range(cores):
+        globals()['p_%d' % i].join()
+
 
 
